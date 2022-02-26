59fb30e174dfed358cd1445a49e399ebf842981c
Luca Leon Happel <luca.happel@uni-duesseldorf.de>
2022-02-26 17:09:51 +0100

    added mons as an external dependency

--- flake.nix
+++ flake.nix
@@ -11,9 +11,11 @@
     let
       pkgs = import nixpkgs { inherit system; };
     in {
-      defaultPackage = pkgs.poetry2nix.mkPoetryApplication {
+      defaultPackage = (pkgs.poetry2nix.mkPoetryApplication {
         projectDir = ./.;
-      };
+      }).overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ [ pkgs.mons ];
+      });
       # packages.x86_64-linux. = nixpkgs.legacyPackages.x86_64-linux.hello;
       # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
     }
